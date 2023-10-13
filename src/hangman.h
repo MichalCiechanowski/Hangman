7d1c2a44bdde8c9d6dabbc87960d1a288ce4c5f2
g180n <ciechanowski62@gmail.com>
2023-10-13 16:58:27 +0200

    File separation

---
+++ src/hangman.h
@@ -0,0 +1,22 @@
+#include <vector>
+#include <string>
+
+class hangman {
+  public:
+    hangman();
+    void game();
+  private:
+    char leter;
+    int guess_count = 0;
+    
+    std::vector<char> guessed_letters;
+    std::string password, players_name;
+    std::string guess_letter;
+
+    std::string password_hider(std::string pass);
+    std::string print_guessed_letters(std::vector<char> leters);
+    
+    void setPassword();
+    void Win();
+    void Loose();
+};
